a2819d65141541c078a8ee43f78cb539bdab0eaa
Samir Singh <ersamirsingh@gmail.com>
2025-10-06 03:04:28 +0530

    Lesson controller and Lesson schema created

---
+++ Server/src/Controllers/LessonController.js
@@ -0,0 +1,123 @@
+const Lesson = require('../Models/Lesson')
+const Course = require('../Models/Course')
+
+
+
+
+
+const addLesson = async (req, res) => {
+
+   try {
+
+      const { courseId, title, videoUrl, resources, duration } = req.body;
+
+      const course = await Course.findById(courseId);
+      if (!course)
+         return res.status(404).json({ message: "Course not found" });
+
+      const lesson = await Lesson.create({
+         courseId,
+         title,
+         videoUrl,
+         resources,
+         duration,
+      });
+
+      // console.log(course?.lessons)
+
+      course.lessons.push(lesson._id);
+      await course.save();
+
+      res.status(201).json({
+         message: "Lesson added successfully",
+         lesson,
+      });
+
+   } catch (error) {
+      res.status(500).json({ 
+         message: error.message 
+      });
+   }
+};
+
+
+
+
+const deleteLesson = async (req, res) => {
+
+
+   try {
+
+      const { id } = req.params;
+      if(!id)
+         return res.status(404).json({
+            message: 'Invalid request'
+         })
+
+      const deletedLesson = await Lesson.findByIdAndDelete(id);
+      if (!deletedLesson)
+         return res.status(404).json({ 
+            message: 'Lesson not found' 
+         });
+      
+
+      await Course.updateOne(
+         { _id: deletedLesson.courseId },
+         { $pull: { lessons: deletedLesson._id } }
+      );
+
+      res.status(200).json({ message: 'Lesson deleted successfully' });
+
+   } catch (error) {
+      console.error(error);
+      res.status(500).json({ 
+         message: 'Internal server error', 
+         error: error.message 
+      });
+   }
+
+};
+
+
+
+
+const updateLesson = async (req, res)=>{
+
+
+   try {
+
+      const data = req.body
+      if(!data)
+         return res.status(400).json({
+            message: 'Nothing for update'
+         })
+      
+      const {id} = req.params
+      if(!id)
+         throw new Error('Invalid request')
+
+      let lesson = await Lesson.findOne({_id:id})
+      if(!lesson)
+         return res.status(404).json({
+            message: 'Lesson not found'
+         })
+
+      lesson = await Lesson.findByIdAndUpdate({_id: id}, data, {new: true});
+      res.status(201).json({
+         message: 'lesson updated successfully',
+         lesson
+      })
+      
+   } catch (error) {
+
+      res.status(500).json({
+         message: "Internal server error",
+         error: error.message
+      })
+      
+   }
+}
+
+
+
+module.exports = {addLesson, deleteLesson, updateLesson}
---
+++ Server/src/Models/Lesson.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+
+const lessonSchema = new Schema({
+
+   courseId: {
+      type: Schema.Types.ObjectId,
+      ref: 'course',
+      required: true,
+   },
+
+   title: {
+      type: String,
+      required: true,
+      trim: true,
+      maxLength: 100,
+   },
+
+   videoUrl: {
+      type: String,
+      required: true,
+   },
+
+   resources: [
+      {
+         type: String, // could be URLs or filenames
+      }
+   ],
+
+   duration: {
+      type: Number, // in minutes
+      required: true,
+   }
+
+}, { timestamps: true });
+
+
+const Lesson = mongoose.model('lesson', lessonSchema);
+module.exports = Lesson;
